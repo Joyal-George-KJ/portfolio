2ffad9dd1da6d838e0803a33626bca91c968c7ee
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-01-16 23:24:38 +0530

    Updates

---
+++ assets/Blog-y-Dyn46i.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Bss7ai37.js";function t(){return o.jsx("div",{children:"Blog"})}export{t as default};
---
+++ assets/Project-BUjirpNN.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Bss7ai37.js";function e(){return r.jsx("div",{children:"Project"})}export{e as default};
