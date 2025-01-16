eb03e3ee6dd7c3a5178c1b5f20a4f9eb9c5c51c1
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-01-16 21:31:09 +0530

    Tailwind CSS Breakpoint adjustment.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            screens: {
+                mobile: "320px", // Mobile devices
+                tablet: "481px", // iPads, Tablets
+                laptop: "769px", // Small screens, laptops
+                desktop: "1025px", // Desktops, large screens
+                tv: "1201px", // Extra large screens, TV
+            },
+        },
+    },
+    plugins: [],
+};
