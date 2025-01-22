b907380faa619e183a772215e4a2f0ea53a80b53
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-01-22 12:28:35 +0530

    Projects Responsive almost done!

--- src/screens/Project.jsx
+++ src/screens/Project.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import GitHubLogo from "../assets/img/github.png";
 function Project() {
     return (
-        <div className="text-neutral-200 p-24">
+        <div className="text-neutral-200 mobile:p-4 laptop:p-24">
             <h2 className="text-4xl pb-12 font-bold">Projects</h2>
             <div className="project-container">
                 <div className="project-card shadow-sm shadow-neutral-300 p-4 rounded-lg backdrop-blur">
@@ -12,20 +12,20 @@ function Project() {
                             <img src={GitHubLogo} alt="GitHub Logo" />
                         </a>
                     </div>
-                    <div className="flex mobile:flex-col tablet:flex-row gap-4 pt-4">
+                    <div className="flex mobile:flex-col laptop:flex-row gap-4 pt-4">
                         <img
-                            className="w-2/4 rounded-lg"
+                            className="mobile:w-full laptop:w-2/4 rounded-lg"
                             src="https://cdn-imgix.headout.com/media/images/c9db3cea62133b6a6bb70597326b4a34-388-dubai-img-worlds-of-adventure-tickets-01.jpg?auto=format&w=1222.3999999999999&h=687.6&q=90&fit=crop&ar=16%3A9&crop=faces"
                             alt=""
                         />
-                        <div className="w-2/5 flex gap-4 flex-col">
-                            <p className="text-xl">Description</p>
-                            <p>
+                        <div className="mobile:w-full laptop:w-2/5 flex gap-4 flex-col">
+                            <p className="mobile:text-lg tablet:text-xl laptop:text-2xl">Description</p>
+                            <p className="w-full">
                                 Lorem ipsum dolor sit amet, consectetur
                                 adipiscing elit. Nullam suscipit, eros vitae
                                 tincidunt tincidunt, nunc libero ultricies
                             </p>
-                            <div className="text-5xl flex gap-2 pt-4 w-max">
+                            <div className="text-5xl flex gap-2 pt-4 w-full flex-wrap">
                                 <i class="devicon-html5-plain-wordmark colored w-max aspect-square border p-4 rounded-lg border-neutral-400"></i>
                                 <i class="devicon-css3-plain-wordmark colored w-max aspect-square border p-4 rounded-lg border-neutral-400"></i>
                                 <i class="devicon-git-plain-wordmark colored w-max aspect-square border p-4 rounded-lg border-neutral-400"></i>
