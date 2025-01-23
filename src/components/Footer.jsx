d45d9468e8ddc5562b03ab417c1e124152c0f6f5
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-01-23 12:40:12 +0530

    Responsive Adjustments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Footer() {
     return (
-            <div className="px-24 mobile:px-4 py-8 shadow shadow-white flex flex-col backdrop-blur">
-                <p className="font-medium text-neutral-100 mobile:text-lg text-xl pb-4 mobile:pb-2">Contact Me</p>
+            <div className="laptop:px-24 mobile:px-8 bg-neutral-800 py-4 shadow shadow-white flex flex-col backdrop-blur">
+                <p className="font-medium text-neutral-100 mobile:text-base laptop:text-xl pb-4 mobile:pb-2">Contact Me</p>
                 <a
-                    className="text-neutral-300 mobile:text-base"
+                    className="text-neutral-300 mobile:text-sm"
                     href="mailto:joyalgeorgekj@gmail.com"
                 >
                     <span className="font-medium text-neutral-300 pr-2">
@@ -14,7 +14,7 @@ function Footer() {
                     joyalgeorgekj@gmail.com
                 </a>
                 <a
-                    className="text-neutral-300 mobile:text-base"
+                    className="text-neutral-300 mobile:text-sm"
                     href="https://www.linkedin.com/in/joyalgeorgekj"
                 >
                     <span className="font-medium text-neutral-300 pr-2">
