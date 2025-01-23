2d217fe3de10df74be4a8c1340c394aa068041f6
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-01-23 12:42:00 +0530

    Blogs Implimentation Started.

--- src/screens/Blog.jsx
+++ src/screens/Blog.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function Blog() {
-  return (
-    <div>Blog</div>
-  )
+    return (
+        <div className="min-h-[100vh]">
+            <h3 className="mobile:px-8 laptop:px-24 text-neutral-200 py-4">Blog</h3>
+            <div className="blog-container text-neutral-200 font-mono flex gap-4 flex-col mobile:px-4 laptop:px-24">
+                <div className="blog border shadow-neutral-300 flex flex-col gap-2 backdrop-blur-md shadow p-4 rounded-lg">
+                    <h4 className="text-lg">Blog 1</h4>
+                    <p className="line-clamp-2">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio molestias vero asperiores perspiciatis, quae in sit quas cumque qui provident?
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Blog
+export default Blog;
