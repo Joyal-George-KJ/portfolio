304650ff818bbc776365bdee221c543c372bf3b9
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-01-26 12:48:33 +0530

    Blog Card - In Progress

--- src/screens/Blog.jsx
+++ src/screens/Blog.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import blogImage from "../assets/img/blog-test-img.jpg";
 
 function Blog() {
     return (
         <div className="min-h-[100vh]">
             <h3 className="mobile:px-8 laptop:px-24 text-neutral-200 py-4">Blog</h3>
-            <div className="blog-container text-neutral-200 font-mono flex gap-4 flex-col mobile:px-4 laptop:px-24">
-                <div className="blog border shadow-neutral-300 flex flex-col gap-2 backdrop-blur-md shadow p-4 rounded-lg">
-                    <h4 className="text-lg">Blog 1</h4>
-                    <p className="line-clamp-2">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio molestias vero asperiores perspiciatis, quae in sit quas cumque qui provident?
-                    </p>
+            <div className="blog-container text-neutral-200 font-mono flex flex-col gap-4 mobile:px-4 laptop:px-24">
+                <div className="blog border shadow-neutral-300 flex flex-row gap-2 backdrop-blur-md shadow p-4 rounded-lg">
+                    <div className="">
+                        <img src={blogImage} height={240} width={240} alt="" />
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <h4 className="text-3xl">Blog 1</h4>
+                        <p className="line-clamp-2">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio molestias vero asperiores perspiciatis, quae in sit quas cumque qui provident?
+                        </p>
+                        <div>
+                            //tags
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
