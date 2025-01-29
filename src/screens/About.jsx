fdfb320d2ea5ace39e0866599904e2fb7ae0154c
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-01-29 13:18:30 +0530

    Blog Almost Done, About Section Started!

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
 
 function About() {
-  return (
-    <div>About</div>
-  )
+    return (
+        <div className="text-neutral-200 font-mono text-lg py-4 min-h-[100vh] mobile:px-6 laptop:px-24">
+            <h1>About</h1>
+            <h3>Joyal George K J</h3>
+            <p>
+                React JS Developer who builds reusable components and hooks for
+                performance and optimized sites. Passionate about learning new
+                techs and building projects that solve real-world problems.
+            </p>
+            <div className="w-full flex justify-center">
+                <div className="grid grid-cols-2 gap-4 laptop:w-2/5 mobile:w-full tablet:w-2/4 text-center backdrop-blur">
+                    <span className="aspect-square hover:text-neutral-800 hover:bg-neutral-200 shadow-neutral-200 justify-center items-center flex shadow rounded ">
+                        Web Development
+                    </span>
+                    <span className="aspect-square hover:text-neutral-800 hover:bg-neutral-200 shadow-neutral-200 justify-center items-center flex shadow rounded ">
+                        Mobile App Development
+                    </span>
+                    <span className="aspect-square hover:text-neutral-800 hover:bg-neutral-200 shadow-neutral-200 justify-center items-center flex shadow rounded ">
+                        Software Development
+                    </span>
+                    <span className="aspect-square hover:text-neutral-800 hover:bg-neutral-200 shadow-neutral-200 justify-center items-center flex shadow rounded ">
+                        SEO Optimization
+                    </span>
+                </div>
+            </div>
+            <p>Tech Stack I use:</p>
+            <div className="text-4xl flex gap-4 flex-wrap bg-neutral-200 p-4 rounded-lg shadow-neutral-200">
+                <i className="devicon-html5-plain-wordmark colored"></i>
+                <i className="devicon-css3-plain-wordmark colored"></i>
+                <i className="devicon-javascript-plain colored"></i>
+                <i className="devicon-react-original-wordmark colored"></i>
+                <i className="devicon-redux-original colored"></i>
+                <i className="devicon-nextjs-original-wordmark colored"></i>
+                <i className="devicon-tailwindcss-original colored"></i>
+                <i className="devicon-github-original-wordmark colored"></i>
+                <i className="devicon-npm-original-wordmark colored"></i>
+                <i className="devicon-postman-plain colored"></i>
+                <i className="devicon-sass-original colored"></i>
+                <i className="devicon-vscode-plain colored"></i>
+            </div>
+        </div>
+    );
 }
 
-export default About
+export default About;
--- src/screens/Blog.jsx
+++ src/screens/Blog.jsx
@@ -6,7 +6,7 @@ function Blog() {
     return (
         <div className="min-h-[100vh]">
             <h3 className="mobile:px-8 font-mono laptop:px-24 mobile:text-lg laptop:text-3xl text-neutral-200 py-4">Blog</h3>
-            <div className="blog-container text-neutral-200 font-mono grid mobile:grid-cols-1 tablet:grid-cols-3 laptop:grid-cols-4 gap-6 mobile:px-4 laptop:px-24">
+            <div className="blog-container text-neutral-200 font-mono grid mobile:grid-cols-1 tablet:grid-cols-3 laptop:grid-cols-4 gap-6 mobile:px-6 laptop:px-24">
                 <BlogCard blogImage={blogImage} />
                 <BlogCard blogImage={blogImage} />
             </div>
