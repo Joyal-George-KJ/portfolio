019ae134b644fc1bf4ab4d5bf94b5464344d57fb
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-02-04 19:28:22 +0530

    compression downloaded

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-})
+    plugins: [
+        react(),
+        compression({ algorithm: "gzip" }), // Enable Gzip compression
+        compression({ algorithm: "brotliCompress" }), // Enable Brotli compression
+    ],
+    base: "/",
+});
