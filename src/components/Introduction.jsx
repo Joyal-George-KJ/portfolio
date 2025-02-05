18fe34d23d86e7ccd9d0b4ac000d4c91cda8d822
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-02-05 12:36:19 +0530

    unwanted class removed

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Introduction() {
     
     return (
-        <div className="bg-gray-900 text-neutral-100 p-8 md:p-16 lg:p-24 shadow-lg transition-transform transform hover:scale-[1.02] rounded-lg">
+        <div className="bg-gray-900 text-neutral-100 p-8 md:p-16 lg:p-24 shadow-lg transition-transform transform hover:scale-[1.02]">
             <h3 className="text-xl md:text-3xl font-bold text-blue-400">Hey there! 👋</h3>
             <p className="text-sm md:text-2xl font-light mt-2">
                 I'm <span className="font-semibold text-blue-300">Joyal George K J</span> from Kochi, Kerala, India.
