479fe532366bf7028f791e7a5361429c5ee8d54b
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-02-06 11:19:40 +0530

    Tiny Adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -59,7 +59,7 @@ function Header() {
                             to="/project"
                             className="hover:text-purple-500 transition"
                         >
-                            Projects
+                            Project
                         </Link>
                         <Link
                             onClick={() => setToggle(false)}
@@ -69,7 +69,7 @@ function Header() {
                             About
                         </Link>
                         <button className="bg-purple-600 text-white px-4 py-2 rounded-lg hover:bg-purple-700 transition">
-                            Login
+                            Resume
                         </button>
                         <ThemeToggle />
                     </div>
@@ -88,7 +88,7 @@ function Header() {
                                 to="/project"
                                 className="hover:text-purple-500 transition duration-300"
                             >
-                                Projects
+                                Project
                             </Link>
                         </li>
                         <li className="cursor-pointer">
@@ -101,7 +101,7 @@ function Header() {
                         </li>
                         <li className="cursor-pointer">
                             <button className="bg-purple-600 text-white px-4 py-2 rounded-lg hover:bg-purple-700 transition duration-300">
-                                Login
+                                Resume
                             </button>
                         </li>
                         <li className="cursor-pointer">
