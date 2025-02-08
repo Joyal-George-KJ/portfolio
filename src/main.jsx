a12f8faf3e66422c8cc93638a3b8a632596147e8
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-02-08 12:59:26 +0530

    Changes Done for Bug Fixing

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import store from './utilities/global state/store.js'
+import store from './utilities/globalState/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
