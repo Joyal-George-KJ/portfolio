61611ad80c06976e651f8b1058faa133f511073c
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-02-08 13:17:46 +0530

    Typo Corrected

--- src/utilities/globalState/store.js
+++ src/utilities/globalState/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./slice/themeSlice";
-import dataReducer from "./slice/DataSlice";
+import dataReducer from "./slice/dataSlice";
 
 const store = configureStore({
     reducer: {
