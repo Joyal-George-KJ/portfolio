3b96533d18348438a34e909263a587221750e4d6
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-04-19 16:43:44 +0530

    UI Adjustment

--- src/screens/Blog.jsx
+++ src/screens/Blog.jsx
@@ -5,7 +5,7 @@ import BlogCard from "../components/BlogCard";
 function Blog() {
     return (
         
-        <div className="font-mono text-lg py-8 min-h-screen laptop:px-24">
+        <div className="font-mono text-lg laptop:py-8 min-h-screen laptop:px-24">
             <div className="relative z-10 p-6 rounded-lg bg-opacity-20">
 
             <h1 className="mobile:text-2xl tablet:text-3xl laptop:text-4xl mobile:border-b tablet:border-b-2 laptop:border-b-4 font-bold dark:text-blue-400 text-blue-600 border-blue-600 dark:border-blue-500 pb-3">Blog</h1>
