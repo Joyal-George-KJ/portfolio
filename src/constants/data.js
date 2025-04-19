8117aa99d22a2cb6e47adf790629c226b8de5772
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-04-19 16:38:54 +0530

    Removed hard coded Experience and Project data

--- src/constants/data.js
+++ src/constants/data.js
@@ -21,58 +21,6 @@ const data = {
         "Appwrite",
         "Redux"
     ],
-    "experience":  [
-        {
-            "year": "8th Grade",
-            "title": "Tech Curiosity Begins",
-            "event": "Explored tech using Termux for ethical hacking experiments.",
-        },
-        {
-            "year": "10th Grade",
-            "title": "Introduction to Computers",
-            "event": "Had computers as a subject but mostly learned how to operate them.",
-        },
-        {
-            "year": "11th - 12th",
-            "title": "First Steps in Programming",
-            "event": "Studied Computer Science and IT, first exposure to programming with C++ and HTML.",
-        },
-        {
-            "year": "BCA (Bachelor’s)",
-            "title": "Software Development & Competitions",
-            "event": "Developed Java, PHP, and Python-based projects. Won 1st prize at a tech fest.",
-        },
-        {
-            "year": "Self-learning Phase",
-            "title": "Deep Dive into Frontend",
-            "event": "Built frontend projects, solved coding challenges on HackerRank & CodeWars, and completed challenges on Frontend Mentor and iCodeThis.",
-        },
-        {
-            "year": "Current",
-            "title": "Pursuing Developer Career",
-            "event": "Working part-time as a fuel attendant while preparing for a full-time developer role.",
-        }
-    ],
-    "projects": [
-        {
-            "type": "Bachelor’s Project Re-Done",
-            "name": "CouponShare",
-            "image": "/CouponShare.png",
-            "description": "CouponShare is a community-powered platform designed to help users find, share, and save with the best discount codes, promo offers, and deals across various categories. Whether it’s shopping, food delivery, subscriptions, or local services, CouponShare enables users to contribute and benefit in real time.",
-            "techStack": ["html", "css", "js", "php", "mysql"],
-            "visitLink": "",
-            "codeLink": "https://github.com/Joyal-George-KJ",
-        },
-        {
-            "type": "Production Grade Project",
-            "name": "EpicLibrary",
-            "image": "/EpicLibrary.png",
-            "description": "EpicLibrary is an entertainment tracking platform that lets users explore and track their favorite anime, movies, TV series, books, and manga—all in one place. Whether you're a binge-watcher or bookworm, EpicLibrary helps you stay organized while discovering new favorites.",
-            "techStack": ["html", "css", "js","react", "tailwind"],
-            "visitLink": "https://www.frontendmentor.io/profile/Joyal-George-KJ",
-            "codeLink": "https://github.com/Joyal-George-KJ",
-        },
-    ],
     "socials": [
         {
             "name": "Instagram",
