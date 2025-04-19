ba14a92fc4cfd52abc44bb6927f76d40e6e04e8c
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-04-19 16:39:54 +0530

    Database data of project is uploaded and rendered

--- src/screens/Project.jsx
+++ src/screens/Project.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import ProjectCard from "../components/ProjectCard";
-import { useSelector } from "react-redux";
+import useAppwrite from "../hooks/useAppwrite";
+import CardLoader from "../components/CardLoader";
 
 function Project() {
-    const { projects } = useSelector((state) => state.data);
+    const { data, loading, error } = useAppwrite(
+        import.meta.env.VITE_PERSONAL_PROJECT_ID
+    );
+
+    if (error) return <p>Something went wrong.</p>;
 
     return (
-        <div className="font-mono text-lg py-8 min-h-fit laptop:px-24">
+        <div className="font-mono text-lg laptop:py-8 min-h-fit laptop:px-24">
             <div className="relative z-10 p-6 rounded-lg bg-opacity-20">
                 <h1 className="mobile:text-2xl tablet:text-3xl laptop:text-4xl mobile:border-b tablet:border-b-2 laptop:border-b-4 font-bold dark:text-blue-400 text-blue-600 border-blue-600 dark:border-blue-500 pb-3">
                     Project
                 </h1>
                 <div className="grid mobile:grid-cols-1 laptop:grid-cols-2 gap-4 mt-6">
-                    {projects.map((val, ind) => (
+                    {(loading || !data) &&
+                        [...Array(2)].map((_, i) => <CardLoader key={i} />)}
+                    {data?.map((val, ind) => (
                         <ProjectCard
                             index={ind + 1}
                             key={ind}
                             name={val.name}
                             description={val.description}
-                            image={
-                                val.image
-                            }
-                            
-                            projectType={val.type}
-                            techStack={val.techStack}
-                            codeLink={val.codeLink}
-                            visitLink={val.visitLink}
+                            image={val.image}
+                            projectType={val.title}
+                            techStack={val.tech_stacks_used}
+                            codeLink={val.code_link}
+                            visitLink={val.preview_link}
                         />
                     ))}
                 </div>
