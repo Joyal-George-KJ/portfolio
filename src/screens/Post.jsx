6abd79235173c7fb86267d5108e993e5f51d4150
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-04-30 01:44:21 +0530

    Post Component done and implimented each url change and post appearence

--- src/screens/Post.jsx
+++ src/screens/Post.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import useAppwrite from "../hooks/useAppwrite";
-import Markdown from "react-markdown";
+import BlogCard from "../components/BlogCard";
+import ProjectCard from "../components/ProjectCard";
 
 function formatDateToDDMMYYYY(dateString) {
     const date = new Date(dateString);
@@ -13,9 +14,11 @@ function formatDateToDDMMYYYY(dateString) {
 }
 
 function Post() {
-    const { id } = useParams();
+    const { id, path } = useParams();
     let { data, loading, error } = useAppwrite(
-        import.meta.env.VITE_PERSONAL_BLOG_ID,
+        path === "blog"
+            ? import.meta.env.VITE_PERSONAL_BLOG_ID
+            : import.meta.env.VITE_PERSONAL_PROJECT_ID,
         id
     );
     if (loading) {
@@ -26,105 +29,89 @@ function Post() {
         );
     }
 
-    console.log(data);
-    
-
-    data.date = formatDateToDDMMYYYY(data?.$createdAt);
-
     return (
         <div className="font-mono text-lg laptop:py-8 min-h-screen laptop:px-24">
             <div className="relative z-10 p-6 rounded-lg bg-opacity-20">
-                <h1 className="mobile:text-2xl tablet:text-3xl laptop:text-4xl mobile:border-b tablet:border-b-2 laptop:border-b-4 font-bold dark:text-blue-400 text-blue-600 border-blue-600 dark:border-blue-500 pb-3">
-                    Blog
-                </h1>
-                <div
-                    className="blog-container mt-6 text-neutral-800 dark:text-neutral-200 font-mono grid mobile:grid-cols-1 laptop:grid-cols-2 tv:grid-cols-3 gap-6"
-                ></div>
-        <div className="dark:bg-gray-800 bg-gray-200 p-5 rounded-2xl shadow-lg transition-transform transform hover:scale-[1.02]">
-            <div className="overflow-hidden rounded-lg">
-                <img
-                    src={import.meta.env.VITE_STORE_URL + data.image}
-                    className="w-full h-12 object-cover rounded-lg"
-                    alt="blog img"
-                />
-            </div>
-
-            <div className="mt-4 dark:text-neutral-200 text-neutral-800">
-                <span className="text-sm text-gray-600 dark:text-gray-400">
-                    {data.date}
-                </span>
-                <h4 className="text-2xl font-semibold text-blue-400 mt-1">
-                    {data.title}
-                </h4>
-                <Markdown
-                    components={{
-                        h1: ({ node, ...props }) => (
-                            <h1
-                                className="text-3xl font-bold mt-6"
-                                {...props}
-                            />
-                        ),
-                        h2: ({ node, ...props }) => (
-                            <h2
-                                className="text-2xl font-semibold mt-4"
-                                {...props}
-                            />
-                        ),
-                        h3: ({ node, ...props }) => (
-                            <h3
-                                className="text-xl font-medium mt-3"
-                                {...props}
-                            />
-                        ),
-                        p: ({ node, ...props }) => (
-                            <p className="text-base mb-4" {...props} />
-                        ),
-                        ul: ({ node, ...props }) => (
-                            <ul className="list-disc ml-6 mb-4" {...props} />
-                        ),
-                        ol: ({ node, ...props }) => (
-                            <ol className="list-decimal ml-6 mb-4" {...props} />
-                        ),
-                        blockquote: ({ node, ...props }) => (
-                            <blockquote
-                                className="border-l-4 pl-4 italic text-gray-600 dark:text-gray-400"
-                                {...props}
-                            />
-                        ),
-                        code: ({ node, ...props }) => (
-                            <code
-                                className="bg-gray-100 dark:bg-gray-800 px-1 rounded"
-                                {...props}
-                            />
-                        ),
-                        pre: ({ node, ...props }) => (
-                            <pre
-                                className="bg-gray-100 dark:bg-gray-800 p-4 rounded overflow-x-auto text-sm"
-                                {...props}
-                            />
-                        ),
-                        a: ({ node, ...props }) => (
-                            <a
-                                className="text-blue-600 hover:underline dark:text-blue-400"
-                                {...props}
-                            />
-                        ),
-                    }}
-                >
-                    {data.description}
-                </Markdown>
-                <div className="flex flex-wrap gap-2 mt-3">
-                    {data.tags.map((tag, index) => (
-                        <span
-                            key={index}
-                            className="dark:bg-gray-700 dark:text-gray-300 bg-gray-300 text-gray-700 px-3 py-1 text-xs rounded-full"
-                        >
-                            {tag}
-                        </span>
-                    ))}
+                <div className="blog-container mt-6 text-neutral-800 dark:text-neutral-200 font-mono grid grid-cols-1 gap-6">
+                    {path === "blog" ? (
+                        <BlogCard
+                            {...data}
+                            full={true}
+                            markdownComponent={{
+                                h1: ({ node, ...props }) => (
+                                    <h1
+                                        className="text-3xl font-bold mt-6"
+                                        {...props}
+                                    />
+                                ),
+                                h2: ({ node, ...props }) => (
+                                    <h2
+                                        className="text-2xl font-semibold mt-4"
+                                        {...props}
+                                    />
+                                ),
+                                h3: ({ node, ...props }) => (
+                                    <h3
+                                        className="text-xl font-medium mt-3"
+                                        {...props}
+                                    />
+                                ),
+                                p: ({ node, ...props }) => (
+                                    <p className="text-base mb-4" {...props} />
+                                ),
+                                ul: ({ node, ...props }) => (
+                                    <ul
+                                        className="list-disc ml-6 mb-4"
+                                        {...props}
+                                    />
+                                ),
+                                ol: ({ node, ...props }) => (
+                                    <ol
+                                        className="list-decimal ml-6 mb-4"
+                                        {...props}
+                                    />
+                                ),
+                                blockquote: ({ node, ...props }) => (
+                                    <blockquote
+                                        className="border-l-4 pl-4 italic text-gray-600 dark:text-gray-400"
+                                        {...props}
+                                    />
+                                ),
+                                code: ({ node, ...props }) => (
+                                    <code
+                                        className="bg-gray-100 dark:bg-gray-800 px-1 rounded"
+                                        {...props}
+                                    />
+                                ),
+                                pre: ({ node, ...props }) => (
+                                    <pre
+                                        className="bg-gray-100 dark:bg-gray-800 p-4 rounded overflow-x-auto text-sm"
+                                        {...props}
+                                    />
+                                ),
+                                a: ({ node, ...props }) => (
+                                    <a
+                                        className="text-blue-600 hover:underline dark:text-blue-400"
+                                        {...props}
+                                    />
+                                ),
+                            }}
+                        />
+                    ) : (
+                        <ProjectCard
+                            id={data.$id}
+                            name={data.name}
+                            description={data.description}
+                            image={data.image}
+                            projectType={data.title}
+                            techStack={data.tech_stacks_used}
+                            codeLink={data.code_link}
+                            visitLink={data.preview_link}
+                        />
+                    )}
                 </div>
             </div>
-        </div></div></div>
+        </div>
     );
 }
 
